81bc8e44f3b467c8316c7353a9de625a1e0000fe
Jason <jason.zubrickEIT@gmail.com>
2026-01-14 11:59:13 -0600

    Day 1: Solution plus utils that include a generator that parses .txt input

--- day01/solution.py
+++ day01/solution.py
@@ -1,13 +1,33 @@
 from utils import Parser_1
 
 if __name__ == "__main__":
+
+#This is the solution for part 1
+    counter = 0 # This is the counter that tracks how many times we end on zero
+    ring = 100 # This represents the number of elements contained in the ring
+    base = 50 # This represents the current element in the ring we are pointing to
+    codes = Parser_1("input.txt")
+
+    for code in codes:
+        base = (base + code) % ring # This determines the new element that we are pointing to in the ring after the rotation
+        if base == 0:
+            counter += 1
+    print(f"Solution for part 1: {counter}")
+
+#This is the solution for part 2
     counter = 0
     ring = 100
     base = 50
     codes = Parser_1("input.txt")
 
     for code in codes:
-        base = (base + code) % ring
-        if base == 0:
-            counter += 1
-print(counter)
+        rotation = base + code # This is the sum of the current element and the rotation.
+        if base == 0: # This is the case when our starting element is 0 and we can easily determine how many times it crosses zero from either the left or the right
+            counter += abs(int((rotation)/ring))
+        else: # This is the case we start at any element that is not 0
+            if rotation <= 0: # This is the case that we are going left and have touched 0 at least once
+                counter += 1 + abs(int((rotation)/ring))
+            else: # This is the case where we are going left or right but have not crossed zero at least once as a result of going left
+                counter += abs(int((rotation)/ring))
+        base = (base + code) % ring # This updates the current element to the correct one
+    print(f"Solution for part 2: {counter}")
--- day01/utils.py
+++ day01/utils.py
@@ -1,15 +1,19 @@
+# This class is a generator to store and parse the input into negative or positive integer numbers
 class Parser_1:
     def __init__(self, txt_path):
+        # Initializes the storage of a list of strings
         self.in_text = []
         with open(txt_path, 'r') as file:
             self.in_text = [line.strip() for line in file.readlines()]
 
     def parse(self, inp):
+        # This function parses the strings into either negative or positive integers
         if inp[:1] == "R":
             return int(inp[1:])
         else:
             return -int(inp[1:])
 
     def __iter__(self):
+        # This is a generator function that returns the parsed value
         for line in self.in_text:
             yield self.parse(line)
